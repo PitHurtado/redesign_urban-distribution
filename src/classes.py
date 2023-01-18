5beb48137e8f6424188db37a62d72dc48377737f
PitHurtado <pitehrh@gmail.com>
2023-01-18 11:29:42 -0500

    update model and classes

--- src/classes.py
+++ src/classes.py
@@ -1,19 +1,19 @@
 class Segment:
     def __init__(self,
-                 id: str,
-                 lon: float, lat: float,
-                 areaKm: float,
-                 avgTickets: float,
-                 avgCustomers: float,
-                 avgPackages: float,
-                 avgPackagesBySales: float,
-                 packages: float,
-                 sales: float,
-                 customers: int,
-                 avgDropSize: float,
-                 setSatellitesCoverage: list[str],
-                 costServedFromDC: int,
-                 ):
+                id: str,
+                lon: float, lat: float,
+                areaKm: float,
+                avgTickets: float,
+                avgCustomers: float,
+                avgPackages: float,
+                avgPackagesBySales: float,
+                packages: float,
+                sales: float,
+                customers: int,
+                avgDropSize: float,
+                setSatellitesCoverage: list[str],
+                costServedFromDC: int,
+                ):
         self.id = id
         self.geographyLocation = (lon, lat)
         self.areaKm = areaKm
@@ -31,14 +31,14 @@ class Segment:
 
 class Satellite:
     def __init__(self,
-                 id: str,
-                 lon: float, lat: float,
-                 distanceFromDC: float,
-                 durationFromDC: float,
-                 durationInTrafficFromDC: float,
-                 costFixed: float,
-                 costPerVehicle: float,
-                 ):
+                id: str,
+                lon: float, lat: float,
+                distanceFromDC: float,
+                durationFromDC: float,
+                durationInTrafficFromDC: float,
+                costFixed: float,
+                costPerVehicle: float,
+                ):
         self.id = id
         self.lon = lon
         self.lat = lat
@@ -52,23 +52,28 @@ class Satellite:
 
 class Customer:
     def __init__(self,
-                 id: str,
-                 lon: float, lat: float,
-                 demand: float,
-                 category: float,
-                 isSmall: bool
-                 ):
+                id: str,
+                lon: float, lat: float,
+                demand: float,
+                category: float,
+                isLow: bool,
+                fee_min_satellite: float,
+                fee_min_dc:float
+                ):
         self.id = str(id)
         self.lon = lon
         self.lat = lat
         self.geographyLocation = (lon, lat)
         self.demand = demand
         self.category = category
-        self.isSmall = isSmall
+        self.isLow = isLow
+        self.fee_min_satellite = fee_min_satellite
+        self.fee_min_dc = fee_min_dc
+
 
 class Vehicle:
     def __init(self,
-               id: str,
-               capacity: float):
+                id: str,
+                capacity: float):
         self.id = str(id)
         self.capacity = capacity
